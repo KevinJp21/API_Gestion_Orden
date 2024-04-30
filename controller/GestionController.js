cc912b502b1bae6958bbafbc293591a523720f54
KevinJp21 <kevinjp821@gmail.com>
2024-04-30 14:30:53 -0500

    .

--- controller/GestionController.js
+++ controller/GestionController.js
@@ -45,6 +45,7 @@ ruta.get('/', function(req, res) {
         let sql="SELECT * FROM customers;"
         conexion.query("SELECT  DISTINCT c.customerNumber,c.customerName, c.contactLastName,c.contactFirstName,c.phone, c.addressLine1, c.addressLine2, c.city, c.state, c.postalCode, c.country, c.salesRepEmployeeNumber, c.creditLimit, o.orderNumber, o.orderDate  FROM customers c JOIN orders o WHERE c.customerNumber = o.customerNumber AND c.customerNumber =?", [req.params.customerNumber],(err,rows)=>{
             if(err) throw err;
+            if(rows.length === 0) res.json({codigo:'-1',mensaje: 'El cliente solicitado no existe'})
             else{
                 res.json(rows)
             }
@@ -55,7 +56,7 @@ ruta.get('/', function(req, res) {
 ruta.get('/ordersCustomers/:customerNumber', function(req, res) {
     conexion.query("SELECT orders.orderNumber, orders.customerNumber FROM orders where customerNumber = ?", [req.params.customerNumber],(err,rows)=>{
         if(err) throw err;
-        if(rows.length === 0) res.json({codigo:'-1',mensaje: 'La orden solicitada no existe'})
+        if(rows.length === 0) res.json({codigo:'-1',mensaje: 'El cliente solicitado no existe'})
         else{
             res.json(rows)
         }
@@ -76,7 +77,7 @@ ruta.get('/ordersCustomers/:customerNumber', function(req, res) {
     ruta.get('/employes/:salesRepEmployeeNumber', function(req, res) {
         conexion.query("SELECT c.customerNumber, c.salesRepEmployeeNumber, e.lastName, e.firstName, e.email FROM customers c JOIN employees e ON c.salesRepEmployeeNumber = e.employeeNumber where salesRepEmployeeNumber = ?", [req.params.salesRepEmployeeNumber],(err,rows)=>{
             if(err) throw err;
-            if(rows.length === 0) res.json({codigo:'-1',mensaje: 'La orden solicitada no existe'})
+            if(rows.length === 0) res.json({codigo:'-1',mensaje: 'El empleado solicitado no existe'})
             else{
                 res.json(rows)
             }
